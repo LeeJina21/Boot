3bc70ef8b3bdf10949cc71210f2df9688fbba841
LeeJina21 <vivid635@naver.com>
2022-03-30 11:23:28 +0900

    text

---
+++ src/main/java/kopo/poly/service/IMelonService.java
@@ -0,0 +1,36 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MelonDTO;
+
+import java.util.List;
+
+public interface IMelonService {
+
+
+    /**
+     * 오늘 수집된 멜론 노래리스트 가져오기
+     */
+    List<MelonDTO> getSongList() throws Exception;
+
+    /**
+     * 멜론 가수별 노래 수 가져오기
+     */
+    List<MelonDTO> getSingerSongCnt() throws Exception;
+
+    /**
+     * 가수의 노래 가져오기
+     *
+     * @return 노래 리스트
+     */
+    List<MelonDTO> getSingerSong() throws Exception;
+
+    /**
+     * 멜론 노래 리스트 한번에 저장하기
+     */
+    int collectMelonSongMany() throws Exception;
+
+    /**
+     * 가수의 노래 삭제하기
+     */
+    int deleteSong() throws Exception;
+}
